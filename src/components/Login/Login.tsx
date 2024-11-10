3be19d53e9e10b55c6551bd650d711133b733839
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-11-10 12:04:25 -0300

    finalized login template

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,26 @@
+import { Box, Button, Flex, FormControl, FormLabel, Input, WrapItem } from "@chakra-ui/react";
 import "./Login.css";
 
 const Login = () => {
     return (
         <>
-        
+        <Box w='28vw' h='42vh' borderWidth='1px' borderRadius='lg' p={4} flexDirection='column' justifyContent='center'>
+            <FormControl>
+                <Flex direction='column' gap='4vh'>
+                    <Flex direction='column'>
+                        <FormLabel>Email</FormLabel>
+                        <Input type='email' />
+                    </Flex>
+                    <Flex direction='column'>
+                        <FormLabel>Senha</FormLabel>
+                        <Input type='password' />
+                    </Flex>
+                    <Flex w='100%' justifyContent='center'>
+                        <Button colorScheme='purple'>Entrar</Button>
+                    </Flex>
+                </Flex>
+            </FormControl>
+        </Box>
         </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Flex } from '@chakra-ui/react'
 import Header from './components/Header/Header.tsx'
+import Login from './components/Login/Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <ChakraProvider>
     <StrictMode>
       <Header/>
+      <Flex justifyContent='center' alignItems='center' h='100%'>
+        <Login/>
+      </Flex>
     </StrictMode>
   </ChakraProvider>
 )
