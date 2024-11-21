fc06ce92b7f0195c0ade398bae9e1cdf0dc907b5
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-11-21 20:30:38 -0300

    adjusted headers style and links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,46 @@
-import { Text } from "@chakra-ui/react";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import "./Header.css";
+import { FaHeart, FaRegCompass, FaUserAlt } from "react-icons/fa";
+import { IoIosLogOut } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+
 
 const Header = () => {
+
+    const navigate = useNavigate()
+
+    const routerChanger = (path:string) => {
+        navigate(path);
+    }
+
     return (
         <>
             <div className="header-conteiner">
                 <div className="header-content">
-                    <Text>Take your actions Now</Text>
+                    <Flex className="conteiner-icon-with-title" flexDirection='row' gap='2vw' alignItems='center' onClick={() =>routerChanger('/home')}>
+                        <Image
+                            boxSize='100px'
+                            objectFit='cover'
+                            src='./src/logo.png'
+                            alt='Dan Abramov'
+                        />
+                        <Text className="header-title">Take your actions Now</Text>
+                    </Flex>
                     <div className="site-actions">
-                        <Text>Explorar</Text>
-                        <Text>Minhas ações</Text>
+                        <Button className="button-conteiner" colorScheme="teal" variant="solid" onClick={() =>routerChanger('/home')}>
+                            <FaRegCompass /> Explorar
+                        </Button>
+                        <Button className="button-conteiner" colorScheme="teal" variant="solid" onClick={() =>routerChanger('/bookmark')}>
+                            <FaHeart /> Minhas ações
+                        </Button>
                     </div>
                     <div className="account-actions">
-                        <Text>Minha conta</Text>
-                        <Text>Logout</Text>
+                        <Button className="button-conteiner" colorScheme="teal" variant="solid">
+                            <FaUserAlt /> Minha conta
+                        </Button>
+                        <Button className="button-conteiner" colorScheme="red" variant="solid" onClick={() =>routerChanger('/')}>
+                            <IoIosLogOut /> Logout
+                        </Button>
                     </div>
                 </div>
             </div>
--- src/main.tsx
+++ src/main.tsx
@@ -11,16 +11,16 @@ import Bookmark from './pages/Bookmark/Bookmark.tsx'
 createRoot(document.getElementById('root')!).render(
   <ChakraProvider>
     <StrictMode>
+      <BrowserRouter>
       <Header/>
       <Flex justifyContent='center' alignItems='center' h='100%' w='100%'>
-        <BrowserRouter>
           <Routes>
             <Route path='/' element={<Login/>}> </Route>
             <Route path='/home' element={<Home/>}> </Route>
             <Route path='/bookmark' element={<Bookmark/>}> </Route>
           </Routes>
-        </BrowserRouter>
       </Flex>
+      </BrowserRouter>
     </StrictMode>
   </ChakraProvider>
 )
