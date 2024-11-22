772ad06cf7b866d351e6f59fff37d38b20f5c45c
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-11-21 21:20:29 -0300

    feat; reinplementado o metodo de request de uma melhor forma e criado login response interface para extrair os resultados do request

--- src/client/LoginGateway.ts
+++ src/client/LoginGateway.ts
@@ -5,13 +5,19 @@ interface LoginRequest {
     password: string;
 }
 
-const login = (email:string, password:string) => {
+export interface LoginResponse {
+    token: string|null
+}
+
+const login = async (email:string, password:string):Promise<LoginResponse | null> => {
     const loginRequestObj:LoginRequest = {'email': email, 'password': password};
-    mercadoFacilClient.post("/api/Login/login", loginRequestObj).then(
-        (resp) => console.log(resp) 
-    ).catch((error) => {
-        console.log("Erro: " + error);
-    });
+    try {
+        const response = await mercadoFacilClient.post("/api/Login/login", loginRequestObj);
+        return response.data as LoginResponse;
+    } catch (errorMessage:any) {
+        console.log(errorMessage);
+        return null;
+    }
 }
 
 export default login;
