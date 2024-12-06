467366d8e9cf1a1cad578dd304686db3cdae691f
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-05 22:41:21 -0300

    feat: added navigate in button 'Detalhes'

--- src/components/ActionCard/ActionCard.tsx
+++ src/components/ActionCard/ActionCard.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Card, CardBody, CardFooter, Image, Stack, Heading, Text, Divider, ButtonGroup, Button } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 const ActionCard = ({ image, name, value }) => {
+
+    const navigate = useNavigate()
+    const handleDetailsClick = () => {
+        navigate(`/actionsDetails/${name}`);
+    };
+
     return (
         <Card maxW="sm">
             <CardBody>
@@ -20,7 +27,7 @@ const ActionCard = ({ image, name, value }) => {
             <Divider />
             <CardFooter>
                 <ButtonGroup spacing="2">
-                    <Button variant="solid" colorScheme="blue">
+                    <Button variant="solid" colorScheme="blue" onClick={handleDetailsClick}>
                         Detalhes
                     </Button>
                     <Button variant="ghost" colorScheme="blue">
