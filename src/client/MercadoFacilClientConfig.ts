fed421dde3770cb08a29e13140af6d0f354fbdb0
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-05 23:07:31 -0300

    fix: replace by latest base URL

--- src/client/MercadoFacilClientConfig.ts
+++ src/client/MercadoFacilClientConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const mercadoFacilClient = axios.create({
-    baseURL: 'https://37b4pnp5-5165.brs.devtunnels.ms'
+    baseURL: 'https://r2jq3bn1-5165.brs.devtunnels.ms'
 });
 
 export default mercadoFacilClient;
