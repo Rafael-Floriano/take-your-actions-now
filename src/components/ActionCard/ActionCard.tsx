e718b2c7e265c89ec20cf0caf56b6e371a2b9900
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-12-05 21:26:08 -0300

    replace mocked values by props values

--- src/components/ActionCard/ActionCard.tsx
+++ src/components/ActionCard/ActionCard.tsx
@@ -1,34 +1,35 @@
-import { Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import React from 'react';
+import { Card, CardBody, CardFooter, Image, Stack, Heading, Text, Divider, ButtonGroup, Button } from '@chakra-ui/react';
 
-const ActionCard = () => {
+const ActionCard = ({ image, name, value }) => {
     return (
-        <Card maxW='sm'>
+        <Card maxW="sm">
             <CardBody>
                 <Image
-                src='https://s3-symbol-logo.tradingview.com/banco-do-brasil--big.svg'
-                alt='Green double couch with wooden legs'
-                borderRadius='lg'
+                    src={image}
+                    alt={`${name} image`}
+                    borderRadius="lg"
                 />
-                <Stack mt='6' spacing='3'>
-                <Heading size='md'>BRASIL ON NM</Heading>
-                <Text color='blue.600' fontSize='2xl'>
-                    R$25,37
-                </Text>
+                <Stack mt="6" spacing="3">
+                    <Heading size="md">{name}</Heading>
+                    <Text color="blue.600" fontSize="2xl">
+                        {value}
+                    </Text>
                 </Stack>
             </CardBody>
             <Divider />
             <CardFooter>
-                <ButtonGroup spacing='2'>
-                <Button variant='solid' colorScheme='blue'>
-                    Detalhes
-                </Button>
-                <Button variant='ghost' colorScheme='blue'>
-                    Adicionar aos favoritos
-                </Button>
+                <ButtonGroup spacing="2">
+                    <Button variant="solid" colorScheme="blue">
+                        Detalhes
+                    </Button>
+                    <Button variant="ghost" colorScheme="blue">
+                        Adicionar aos favoritos
+                    </Button>
                 </ButtonGroup>
             </CardFooter>
         </Card>
-    )
-}
+    );
+};
 
 export default ActionCard;
